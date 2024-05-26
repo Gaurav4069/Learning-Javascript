66a64a32b8586ca026c7768bb6fce34c3222b9bf
Gaurav <149134529+Gaurav4069@users.noreply.github.com>
2024-05-26 06:57:04 +0000

    summary added

--- datatypes.js
+++ datatypes.js
@@ -38,3 +38,41 @@ console.log(1+2+"2")  // here number comes first so it will consider the first t
 // the result is like this because comparison and equality check works differently  , comparisons convert null to a number 
 
 // we use === for strict check it checks the values as well as the data type
+
+
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
