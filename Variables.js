6ca71d09711bcb7691336fc1c35910ae75b8b7b5
Gaurav <149134529+Gaurav4069@users.noreply.github.com>
2024-05-26 05:20:05 +0000

    variables done

---
+++ Variables.js
@@ -0,0 +1,12 @@
+// console.log("hello world")
+
+let accountId =50
+var accountName="Gaurav"
+const account =50
+
+console.table([account,accountId,accountName])
+
+let Name 
+var user
+
+console.table([Name,user])
