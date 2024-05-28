3fb871a89a40e31b0a8bccc431de18bba2546a5b
Gaurav <149134529+Gaurav4069@users.noreply.github.com>
2024-05-28 02:45:30 +0000

    Dates completed

---
+++ Maths_Dates.js
@@ -0,0 +1,70 @@
+const balance = new Number(100)  //here we are specifying that it is a number
+// console.log(balance);
+
+// console.log(balance.toString().length);  // these are the methods associated with number
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.8966
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++++++++++ Maths +++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //this gives the absolute value
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 3, 6, 8));
+
+// console.log(Math.random());
+// console.log((Math.random()*10) + 1);
+// console.log(Math.floor(Math.random()*10) + 1);
+
+
+// way of generating a random number between a given range say ( min,max)
+const min = 10
+const max = 20
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+
+
+// Dates
+
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1);
+console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+
+//here we can customize the tolocale string method of the date
+newDate.toLocaleString('default', {
+    weekday: "long",
+    
+})
